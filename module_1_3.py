e00468e807ec0a7bbdc123ee998dda0bcb526972
UserBox007 <number.two.gm@gmail.com>
2024-11-09 19:26:12 +0500

    ку

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = "Андрей"
+print("Имя " + name)
+
+name = 44
+print("Возраст " + str(name))
+
+name += 44
+print("Двойной возраст " + str(name))
+
+is_student  = True
+
+print("Студент? " + str(is_student))
