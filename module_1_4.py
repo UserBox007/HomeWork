4af5807bc882b1941129dacd52b4828979f500df
UserBox007 <number.two.gm@gmail.com>
2024-11-10 19:55:02 +0500

    ку3

---
+++ module_1_4.py
@@ -0,0 +1,29 @@
+
+
+Number_Of_Completed_DZ = 12
+
+Number_Of_Hours_Spent = 1.5
+
+Course_Name = 'Python'
+
+Time_Per_Task = Number_Of_Completed_DZ * Number_Of_Hours_Spent
+
+
+
+#Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+
+
+
+print("Курс: " + Course_Name, ", врего задач: " + Number_Of_Completed_DZ, ", затрачено часов: " + Number_Of_Hours_Spent,  ", среднее время выполнения: " + Time_Per_Task)
+
+name = 44
+print("Возраст " + str(name))
+
+name += 44
+print("Двойной возраст " + str(name))
+
+is_student  = True
+
+print("Студент? " + str(is_student))
+
+# version 3
