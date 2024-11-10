6c55c674ac8a7709ffdab61884dfbdd1bce5994a
UserBox007 <number.two.gm@gmail.com>
2024-11-10 20:02:54 +0500

    ку3

--- module_1_4.py
+++ module_1_4.py
@@ -2,7 +2,7 @@ Number_Of_Completed_DZ = 12
 Number_Of_Hours_Spent = 1.5
 Course_Name = 'Python'
 
-Time_Per_Task = Number_Of_Completed_DZ * Number_Of_Hours_Spent
+Time_Per_Task = Number_Of_Hours_Spent / Number_Of_Completed_DZ
 
 print("Курс: " + Course_Name, ", врего задач: " + str(Number_Of_Completed_DZ), ", затрачено часов: " + str(Number_Of_Hours_Spent),  ", среднее время выполнения: " + str(Time_Per_Task))
 # version 3
