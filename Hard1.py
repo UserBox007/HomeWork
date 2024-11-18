3145f7d44a4b3cc186831eaabe3cc06959a8d2a6
UserBox007 <number.two.gm@gmail.com>
2024-11-18 20:19:06 +0500

    ку3

--- Hard1.py
+++ Hard1.py
@@ -1,1 +1,10 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_list = list(students)
+students_list.sort()
+
+
+students2 = {grades:  students_list}
+print(students2)
+print(students_list)
+print(sum(grades[1])/ len(grades[1]))
