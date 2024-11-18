fb54c3cc6ce71558b051fe6117c8ada3e64a34f0
UserBox007 <number.two.gm@gmail.com>
2024-11-18 19:33:48 +0500

    ку3

--- module_1_50.py
+++ module_1_50.py
@@ -1,6 +1,12 @@
-my_string = input('Введи строку ')
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(" ", ""))
-print(my_string[0])
-print(my_string[len(my_string)-1])
+immutable_var = (1, 'dsdsds', 4, (3,4))
+print(immutable_var)
+
+# immutable_var[1] = 'www'
+# TypeError: 'tuple' object does not support item assignment
+
+mutable_list = [2,4,4,'sqsqs',2]
+print(f'Оригинальный лист {mutable_list}')
+
+mutable_list[1] = '3'
+mutable_list[3] = 'ДругойЭлемент'
+print(f'Результат замены {mutable_list}')
