8b0944ee337692249e436d75338df39a89a4d3b5
UserBox007 <number.two.gm@gmail.com>
2024-12-12 20:47:21 +0500

    ку3

---
+++ les 3/Hard3.py
@@ -0,0 +1,33 @@
+def calculate_structure_sum(data_structure):
+    result = 0
+    if isinstance(data_structure, int):
+        result += data_structure
+    elif isinstance(data_structure, str):
+        result += len(data_structure)
+    else:
+        for val in data_structure:
+            if isinstance(data_structure, dict):
+                result += calculate_structure_sum(data_structure[val])
+            if isinstance(val, int):
+                result += val
+            elif isinstance(val, str):
+                result += len(val)
+             else:
+                result += calculate_structure_sum(val)
+
+    return result
+
+
+
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
+
+
